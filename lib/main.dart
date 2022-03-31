037acebd7504adff6651eebd3288fad8cade0358
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-03-30 23:50:12 -0500

    Main - Avatar Picture Url

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,33 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class Whatsapp extends StatefulWidget {
+  Whatsapp({Key? key}) : super(key: key);
+
+  @override
+  State<Whatsapp> createState() => _WhatsappState();
+}
+
+class _WhatsappState extends State<Whatsapp> {
+  //Adding the main color of the app
+  Color mainColor = Color(0xFF177767);
+  var containerRadius = Radius.circular(30.0);
+  //Adding a list of image URL to simulate the avatar picture
+  List<String> imageUrl = [
+        "https://i.pinimg.com/originals/2e/2f/ac/2e2fac9d4a392456e511345021592dd2.jpg",
+    "https://randomuser.me/api/portraits/men/86.jpg",
+    "https://randomuser.me/api/portraits/women/80.jpg",
+    "https://randomuser.me/api/portraits/men/43.jpg",
+    "https://randomuser.me/api/portraits/women/49.jpg",
+    "https://randomuser.me/api/portraits/women/45.jpg",
+    "https://randomuser.me/api/portraits/women/0.jpg",
+    "https://randomuser.me/api/portraits/women/1.jpg",
+    "https://randomuser.me/api/portraits/men/0.jpg"
+  ];
+  ]
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
