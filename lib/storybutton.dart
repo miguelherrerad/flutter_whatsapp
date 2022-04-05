1a2dce90d8862afef77569a4f125bacd03c2be8e
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-04-05 13:57:03 -0500

    StoryButton - Padding

--- lib/storybutton.dart
+++ lib/storybutton.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-Widget storyButton(String imgUrl, String userName) {}
+Widget storyButton(String imgUrl, String userName) {
+  return Padding(padding: const EdgeInsets.only(right: 10.0),
+  child: Column(
+    children: [
+      CircleAvatar(
+        backgroundImage: NetworkImage,
+      )
+    ],
+  ),)
+}
