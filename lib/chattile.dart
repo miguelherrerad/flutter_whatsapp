fc5798c48ae2f63243ccf32eb1a48af2d1655bcf
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-04-05 14:35:09 -0500

    chatTile - Finish

--- lib/chattile.dart
+++ lib/chattile.dart
@@ -18,11 +18,47 @@ Widget chatTile(
             width: 8.0,
           ),
           Expanded(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [],
-          ))
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        userName,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Text(date),
+                  ],
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(child: Text(userName)),
+                    if (seen)
+                      Icon(
+                        Icons.check_circle,
+                        size: 18.0,
+                        color: Colors.green,
+                      ),
+                    if (!seen)
+                      Icon(
+                        Icons.check_circle_outline,
+                        color: Colors.grey,
+                        size: 18.0,
+                      ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     ),
