1de7f7cee95e7bc324193d94eea5f438034a929b
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-04-05 14:14:12 -0500

    chatTile - InkWell

--- lib/chattile.dart
+++ lib/chattile.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
 
 Widget chatTile(
-    String imgUrl, String userName, String msg, String date, bool seen) {}
+    String imgUrl, String userName, String msg, String date, bool seen) {
+      return InkWell(
+        onTap: () {},
+        child: Padding(padding: const EdgeInsets.symmetric(vertical: 12.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [],
+        ),
+
+        ),
+      )
+    }
